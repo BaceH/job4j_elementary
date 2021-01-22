8ee2e41ac69c0d5d8b2276e93cb2b317df7aaee3
VasylB <vasiliy.byalyk@dstarlab.com>
2021-01-22 11:38:04 +0200

    3. Прерывание нити [#454091]

---
+++ chapter_007/src/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable{
+
+    @Override
+    public void run() {
+        char[] process = new char[]{'|', '/', '-', '\\'};
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process[i]);
+            i++;
+            if (i == 4)
+                i = 0;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+//                e.printStackTrace();
+
+            }
+
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+
+        Thread.sleep(1000);
+        progress.interrupt();
+    }
+
+}
