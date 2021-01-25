e9b78826b50af951263486340681b227b984a04b
VasylB <vasiliy.byalyk@dstarlab.com>
2021-01-25 14:55:14 +0200

    2. Модель памяти Java [#454094] volatile

--- chapter_007/src/ru/job4j/DCLSingleton.java
+++ chapter_007/src/ru/job4j/DCLSingleton.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class DCLSingleton {
-    private static  DCLSingleton inst;
+    private static volatile DCLSingleton inst;
 
     public static DCLSingleton instOf() {
         if (inst == null) {
