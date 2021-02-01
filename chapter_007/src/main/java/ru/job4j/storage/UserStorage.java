a0bec2ca486b3bbb51ec2efa0fb2d0b8e26efef5
VasylB <vasiliy.byalyk@dstarlab.com>
2021-02-01 09:29:10 +0200

    3. Класс хранилища пользователей UserStorage [#454102] fix

--- chapter_007/src/main/java/ru/job4j/storage/UserStorage.java
+++ chapter_007/src/main/java/ru/job4j/storage/UserStorage.java
@@ -13,48 +13,33 @@ public class UserStorage {
     private final AtomicInteger id = new AtomicInteger();
 
     public boolean add (User user){
-        if (users.containsKey(user.getId())){
-            return false;
-        }else {
-            users.put(id.incrementAndGet(), user);
-            return true;
-        }
+        return users.putIfAbsent(id.incrementAndGet(), user) == null;
     }
 
     public synchronized boolean update(User user){
-        if (users.containsKey(user.getId())){
-            users.put(user.getId(), user);
-            return true;
-        }else {
-            return false;
-        }
+        return users.replace(user.getId(), user) == null;
     }
 
     public synchronized boolean delete(User user){
-        if (users.containsKey(user.getId())){
-            users.remove(user.getId());
-            return true;
-        }else {
-            return false;
-        }
+        return users.remove(user.getId(), user);
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount){
 
         if (!users.containsKey(fromId) || !users.containsKey(toId) || users.get(fromId).getAmount() < amount) {
             return false;
-        } else {
-
-            User fromUser = users.get(fromId);
-            fromUser.setAmount(fromUser.getAmount() - amount);
-            users.put(fromId, fromUser);
-
-            User toUser = users.get(toId);
-            toUser.setAmount(toUser.getAmount() + amount);
-            users.put(toId, fromUser);
-
-            return true;
         }
+
+        User fromUser = users.get(fromId);
+        fromUser.setAmount(fromUser.getAmount() - amount);
+        users.put(fromId, fromUser);
+
+        User toUser = users.get(toId);
+        toUser.setAmount(toUser.getAmount() + amount);
+        users.put(toId, fromUser);
+
+        return true;
+
     }
 
 }
