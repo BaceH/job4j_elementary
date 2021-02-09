aa1dddaf25a5fc20a3049d93f1afa90f332a7902
VasylB <vasiliy.byalyk@dstarlab.com>
2021-02-09 17:31:31 +0200

    first cache

---
+++ chapter_007/src/test/java/ru/job4j/cache/CacheTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.cache;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class CacheTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
